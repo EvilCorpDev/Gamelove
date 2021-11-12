519c28df6caae75a7827bad2fa931532847cca82
Zakhar Kliap <zakhar.kliap@ext.hellofresh.com>
2021-11-12 18:33:56 +0200

    add missed no args constructor

--- src/main/java/org/duckdns/androidghost77/gamelove/dto/GameResponse.java
+++ src/main/java/org/duckdns/androidghost77/gamelove/dto/GameResponse.java
@@ -2,8 +2,10 @@ package org.duckdns.androidghost77.gamelove.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class GameResponse {
     private String id;
